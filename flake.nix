78356bb0f8a6f706841bf8d6e5fd11583c655047
Shane Sveller <shane@sveller.dev>
2022-06-27 08:46:40 -0500

    feat(workspace): Initialize Nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nix-based BEAM toolchain management";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      imports = [ ];
+      systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+    };
+}
