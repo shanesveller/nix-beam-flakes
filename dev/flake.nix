c8abe8d77361b96bac2425eacadf6e30bf944faa
Shane Sveller <shanesveller@gmail.com>
2022-06-27 09:15:29 -0500

    feat(workspace): Configure pre-commit hooks for contributors

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,7 +4,13 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pre-commit = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
@@ -21,7 +27,24 @@
         system,
         ...
       }: {
-        devShells.default = pkgs.mkShell {buildInputs = [];};
+        checks = {
+          pre-commit-check = inputs.pre-commit.lib.${system}.run {
+            src = ./..;
+            hooks = {
+              alejandra.enable = true;
+              nix-linter.enable = false;
+              nixfmt.enable = false;
+              nixpkgs-fmt.enable = false;
+              prettier.enable = true;
+              statix.enable = false;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [pkgs.pre-commit];
+          inherit (config.checks.pre-commit-check) shellHook;
+        };
 
         packages.gcroot =
           pkgs.linkFarmFromDrvs "beam-overlay-dev"
