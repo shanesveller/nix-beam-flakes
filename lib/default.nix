b7a9d7a67ebc40edb3236aeffc728b8c96b82906
Shane Sveller <shanesveller@gmail.com>
2022-07-29 13:25:16 -0500

    feat(lib): Create functions to generate Elixir/Erlang derivations

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,15 @@
 {lib}: let
+  inherit (findBasePackage) elixirBasePackage otpBasePackage;
   findBasePackage = import ./findBasePackage.nix {inherit lib;};
+
+  mkElixir = beamPkgs: version: sha256: let
+    basePkg = elixirBasePackage beamPkgs version;
+  in
+    basePkg.override {inherit sha256 version;};
+
+  mkErlang = pkgs: version: sha256: let
+    basePkg = otpBasePackage pkgs version;
+  in
+    basePkg.override {inherit sha256 version;};
 in {
 }
