5e1618d3ff911d7239d83ce7322495f2db20cd8c
Shane Sveller <shanesveller@gmail.com>
2022-08-03 16:33:53 -0500

    feat(packages): Include example devShells to consume generated package sets

--- parts/devshells.nix
+++ parts/devshells.nix
@@ -1,5 +1,30 @@
-{...}: {
-  perSystem = {pkgs, ...}: {
-    devShells.default = pkgs.mkShell {buildInputs = [];};
+{self, ...}: {
+  perSystem = {pkgs, ...}: let
+    mkBeamShell = pkgSet:
+      pkgs.mkShell {
+        packages = with pkgSet; [elixir elixir_ls erlang erlang-ls];
+
+        shellHook = ''
+          elixir --version
+          erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell
+        '';
+      };
+  in {
+    devShells.asdf = let
+      pkgSet = self.lib.packageSetFromToolVersions pkgs ../test/.tool-versions {
+        languageServers = true;
+      };
+    in
+      mkBeamShell pkgSet;
+
+    devShells.example = let
+      pkgSet = self.lib.mkPackageSet {
+        inherit pkgs;
+        elixirVersion = "1.13.4";
+        erlangVersion = "24.3.4.2";
+        languageServers = true;
+      };
+    in
+      mkBeamShell pkgSet;
   };
 }
