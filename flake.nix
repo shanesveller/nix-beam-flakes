d522aeb5c81350c9438e48bf6934c21c8b7a4859
Shane Sveller <shane@sveller.dev>
2022-11-14 09:30:22 -0600

    feat(flakes): Add flake templates

--- flake.nix
+++ flake.nix
@@ -16,8 +16,22 @@
       systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
 
       flake = {
+        flakeModule = ./parts/all-parts.nix;
+
         perSystem = {pkgs, ...}: {
           formatter = pkgs.alejandra;
+        };
+
+        templates = {
+          default = {
+            path = ./templates/default;
+            description = "An environment suitable for developing Elixir applications";
+          };
+
+          phoenix = {
+            path = ./templates/phoenix;
+            description = "An environment suitable for developing Phoenix 1.7 applications";
+          };
         };
       };
     };
