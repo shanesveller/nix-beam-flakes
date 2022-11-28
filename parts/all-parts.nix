ee3c5cc1a0b9ac10079454634974d65df0383aee
Shane Sveller <shane@sveller.dev>
2022-11-28 16:16:49 -0600

    feat(module): Allow module to inject LSP servers into pkgset

--- parts/all-parts.nix
+++ parts/all-parts.nix
@@ -3,6 +3,7 @@
     ./lib.nix
     ./config.nix
     ./devshell.nix
+    ./language-server.nix
     ./packages.nix
   ];
 }
---
+++ parts/language-server.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  lib,
+  flake-parts-lib,
+  ...
+}: let
+  inherit
+    (lib)
+    mkEnableOption
+    mkMerge
+    mkIf
+    ;
+  inherit
+    (flake-parts-lib)
+    mkPerSystemOption
+    mkSubmoduleOptions
+    ;
+in {
+  options = {
+    perSystem = mkPerSystemOption (_: {
+      _file = ./language-server.nix;
+
+      options.beamWorkspace.devShell = mkSubmoduleOptions {
+        languageServers = {
+          elixir = mkEnableOption "elixir-ls";
+          erlang = mkEnableOption "erlang-ls";
+        };
+      };
+    });
+  };
+
+  config = {
+    perSystem = {
+      config,
+      pkgs,
+      ...
+    }: let
+      cfg = config.beamWorkspace;
+    in {
+      beamWorkspace.devShell.packages = mkMerge [
+        (mkIf cfg.devShell.languageServers.erlang [
+          cfg.packages.erlang-ls
+        ])
+        (mkIf cfg.devShell.languageServers.elixir [
+          cfg.packages.elixir_ls
+        ])
+      ];
+    };
+  };
+}
