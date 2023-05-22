643302b86a7a5268bcf9ec271631aac51b3a7a89
Shane Sveller <shane@sveller.dev>
2023-05-22 12:18:33 -0500

    chore(workspace): Switch from nixos-unstable to nixpkgs-unstable

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     pre-commit = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.flake-utils.follows = "flake-utils";
